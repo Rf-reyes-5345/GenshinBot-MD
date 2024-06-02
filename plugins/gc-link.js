bdd9ed999fc0063b7dd228c52cf6acc873ef2e34
❍⌇─➭⌗尺ℯγℯᔆ: ๑ ˚ ͙۪۪̥ ⌨ᵒᶠᶜ <145686835+Rf-reyes-5345@users.noreply.github.com>
2024-06-01 22:37:49 -0400

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, args}) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
        title: '𝐿𝑖𝑛𝑘 𝐺𝑟𝑜𝑢𝑝',
-      body: '𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯🍁🌱',    
+      body: '𝕲𝖊𝖓𝖘𝖍𝖎𝖓-𝕭𝖔𝖙-𝕸𝕯🍁🌱',    
       previewType: 0, thumbnail: fs.readFileSync('./Menu.png'),
  sourceUrl: `https://atom.bio/sakuralite`}}});
 };     
